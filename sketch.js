642a5df5b43826810dad70a0a9296d38b48665c9
b077vg <57477947+b077vg@users.noreply.github.com>
2020-09-16 12:38:20 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 let w=300,h=300; //only 10 
 let img;
+let canvas;
+
 function setup() {  
-	createCanvas(w,h);
+	canvas=createCanvas(w,h);
+	canvas.parent('canvas');
 	img = loadImage("data/picture1/Movie100.jpg");
 }
 
